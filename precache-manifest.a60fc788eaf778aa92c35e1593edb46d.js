98e1972a7935fa6676b16b33d5ff20400aaad612
Abhishek Thory <abhithory@gmail.com>
2021-11-25 21:56:00 +0530

    Updates

---
+++ precache-manifest.a60fc788eaf778aa92c35e1593edb46d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "907557757b61d5de93e8",
+    "url": "/youtube_game/static/css/main.6ccfeaae.chunk.css"
+  },
+  {
+    "revision": "907557757b61d5de93e8",
+    "url": "/youtube_game/static/js/main.90755775.chunk.js"
+  },
+  {
+    "revision": "b24127e5a5a23be5a44f",
+    "url": "/youtube_game/static/css/1.da9ed715.chunk.css"
+  },
+  {
+    "revision": "b24127e5a5a23be5a44f",
+    "url": "/youtube_game/static/js/1.b24127e5.chunk.js"
+  },
+  {
+    "revision": "4eb07a73a5c77ee952ec",
+    "url": "/youtube_game/static/js/runtime~main.4eb07a73.js"
+  },
+  {
+    "revision": "24e9fdc90cf6e3bf9605465111772d67",
+    "url": "/youtube_game/static/media/banner.24e9fdc9.png"
+  },
+  {
+    "revision": "e07b538aa51b6fa77f32828af21cb591",
+    "url": "/youtube_game/static/media/bootstrap-icons.e07b538a.woff2"
+  },
+  {
+    "revision": "7fb23f3b56a834d9dcf615b5e7c78da8",
+    "url": "/youtube_game/static/media/bootstrap-icons.7fb23f3b.woff"
+  },
+  {
+    "revision": "79dbd24eb6472211fa6061c20020f017",
+    "url": "/youtube_game/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/youtube_game/precache-manifest.3e315d10b1d0e19383e878d0250f5b30.js"
+  "/youtube_game/precache-manifest.a60fc788eaf778aa92c35e1593edb46d.js"
 );
 
 workbox.clientsClaim();
